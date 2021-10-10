9c8a8ea4bdaf10c11e3715d378e6a8cd15fe73e7
yytang26 <yytangtangyy@outlook.com>
2021-10-10 12:44:59 +0800

    all

---
+++ guns-separation/guns-base-support/guns-system/src/main/java/cn/stylefeng/guns/sys/modular/user/controller/Test.java
@@ -0,0 +1,9 @@
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+public class Test {
+    public static void main(String[] args) {
+        PageHelper.startPage(1,10);
+        PageInfo pageInfo = new PageInfo()
+    }
+}
